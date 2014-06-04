ae8f2587b6afe13cff8fd538fe4d7e85c1f58a39
louizzel <zelogalesco@gmail.com>
2014-06-04 18:02:03 +0800

    June 4, 2014 Final Commit

--- RetrieveTwitterFeeds/Controllers/Api/GetTweetsNoDBController.cs
+++ RetrieveTwitterFeeds/Controllers/Api/GetTweetsNoDBController.cs
@@ -12,7 +12,7 @@ namespace RetrieveTwitterFeeds.Controllers.Api
 {
     public class GetTweetsNoDBController : ApiController
     {
-        private readonly  ApplicationOnlyAuthorizer _auth = new ApplicationOnlyAuthorizer();
+        private readonly ApplicationOnlyAuthorizer _auth = new ApplicationOnlyAuthorizer();
 
         public List<Status> Get(string query)
         {
@@ -26,122 +26,15 @@ namespace RetrieveTwitterFeeds.Controllers.Api
 
                 _auth.Authorize();
 
-                using (var tweetsDb = new TweetsDBEntities())
+                using (var twitterContext = new TwitterContext(_auth))
                 {
-                    var maxId = "0";
-                    var checker = true;
-                    var returnResult = new List<Status>();
-
-                    var resultForSinceIdInDB = (from table in tweetsDb.Tweets where table.Query.Equals(query) select table).OrderByDescending(m => m.StatusID).FirstOrDefault();
-                    var sinceId = (resultForSinceIdInDB == null) ? "0" : resultForSinceIdInDB.StatusID;
+                    return (from search in twitterContext.Search
+                            where search.Type == SearchType.Search && search.Count == 1000
+                                      && search.Query == query //&& search.SinceID == 474127608817651712 
+                                  select search).SingleOrDefault().Statuses;
                     
-                    var resultForMaxIdInDB = (from table in tweetsDb.Tweets where table.Query.Equals(query) select table).OrderBy(m => m.StatusID).FirstOrDefault();
-                    var maxIdInDB = (resultForMaxIdInDB == null) ? "0" : resultForMaxIdInDB.StatusID;
-
-                    var newSearch = (resultForSinceIdInDB == null) ? true : false;
-
-                    using (var twitterContext = new TwitterContext(_auth))
-                    {
-                        while(checker)
-                        {
-                            if (newSearch)
-                            {
-                                var current = (from search in twitterContext.Search
-                                               where
-                                                   search.Type == SearchType.Search
-                                                   && search.Query == query
-                                                   && search.Count == 100
-                                                   && search.MaxID == ulong.Parse(maxId)
-                                               select search).Single().Statuses;
-
-                                if (current != null)
-                                {
-                                    if (current.Count > 0)
-                                    {
-                                        foreach (var each in current)
-                                        {
-                                            if (ulong.Parse(each.StatusID) < ulong.Parse(sinceId))
-                                            {
-                                                checker = false;
-                                                break;
-                                            }
-                                            if ((from table in tweetsDb.Tweets where table.Query.Equals(query) && table.StatusID.Equals(each.StatusID) select table).SingleOrDefault() == null)
-                                            {
-                                                tweetsDb.Tweets.Add(new Tweets
-                                                {
-                                                    Content = each.Text,
-                                                    CreatedAt = each.CreatedAt,
-                                                    Language = each.Lang,
-                                                    Name = each.User.Name,
-                                                    Query = query,
-                                                    StatusID = each.StatusID,
-                                                    TweetLink = "https://twitter.com/" + each.User.Identifier.ScreenName + "/statuses/" + each.StatusID,
-                                                    UserID = each.User.Identifier.ID,
-                                                    Username = each.User.Identifier.ScreenName,
-                                                    DateInsertedInDB = DateTime.Now
-                                                });
-                                                tweetsDb.SaveChanges();
-                                            }
-                                        }
-                                        maxId = (ulong.Parse(current[current.Count - 1].StatusID) - 1).ToString(CultureInfo.InvariantCulture);
-                                    }
-                                    else
-                                    {
-                                        checker = false;
-                                    }
-                                    returnResult.AddRange(current);
-                                }
-                            }
-                            else //if this is not a new search
-                            {
-                                var current = (from search in twitterContext.Search
-                                               where
-                                                   search.Type == SearchType.Search
-                                                   && search.Query == query
-                                                   && search.Count == 100
-                                                   && search.MaxID == ulong.Parse(maxId)
-                                               select search).Single().Statuses;
-                                if (current != null)
-                                {
-                                    var currentResult = new List<Status>();
-
-                                    if (current.Count > 0)
-                                    {
-                                        foreach (var each in current)
-                                        {
-                                            if (ulong.Parse(each.StatusID) <= ulong.Parse(sinceId))
-                                            {
-                                                checker = false;
-                                                break;
-                                            }
-                                            if ((from table in tweetsDb.Tweets where table.Query.Equals(query) && table.StatusID.Equals(each.StatusID) select table).SingleOrDefault() == null)
-                                            {
-                                                tweetsDb.Tweets.Add(new Tweets
-                                                {
-                                                    Content = each.Text,
-                                                    CreatedAt = each.CreatedAt,
-                                                    Language = each.Lang,
-                                                    Name = each.User.Name,
-                                                    Query = query,
-                                                    StatusID = each.StatusID,
-                                                    TweetLink = "https://twitter.com/" + each.User.Identifier.ScreenName + "/statuses/" + each.StatusID,
-                                                    UserID = each.User.Identifier.ID,
-                                                    Username = each.User.Identifier.ScreenName,
-                                                    DateInsertedInDB = DateTime.Now
-                                                });
-                                                tweetsDb.SaveChanges();
-
-                                                currentResult.Add(each);
-                                            }
-                                        }
-                                    }
-                                    returnResult.AddRange(currentResult);                                    
-                                }
-                            }
-                        }
-                        return returnResult;
-                    };
                 };
+
             }
             catch (Exception e)
             {
@@ -149,88 +42,15 @@ namespace RetrieveTwitterFeeds.Controllers.Api
             }
         }
 
-        public List<Status> Get(string query, string sinceId)
-        {
-            try
-            {
-                if (_auth.BearerToken == null)
-                {
-                    _auth.Credentials = new InMemoryCredentials
-                    {
-                        ConsumerKey = System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"],
-                        ConsumerSecret = System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"]
-                    };
-
-                    if (!_auth.IsAuthorized)
-                        _auth.Authorize();
-                }
-
-                var checker = true;
-                var maxId = "0";
-                var additionalTweets = new List<Status>();
-
-                using (var tweetsDb = new TweetsDBEntities())
-                {
-                    using (var twitterContext = new TwitterContext(_auth))
-                    {
-                        while (checker)
-                        {
-                            /*** This is just a workaround because the sinceid in the query is not working. */
-
-                            var current = (from search in twitterContext.Search
-                                           where
-                                               search.Type == SearchType.Search
-                                               && search.Query == query
-                                               && search.Count == 100
-                                               && search.MaxID == ulong.Parse(maxId)
-                                           select search).Single().Statuses;
-
-                            if (current != null)
-                            {
-                                if (current.Count > 0)
-                                {
-                                    foreach (var each in current)
-                                    {
-                                        if (ulong.Parse(each.StatusID) <= ulong.Parse(sinceId))
-                                        {
-                                            checker = false;
-                                            break;
-                                        }
-                                        if ((from table in tweetsDb.Tweets where table.Query.Equals(query) && table.StatusID.Equals(each.StatusID) select table).SingleOrDefault() == null)
-                                        {
-                                            additionalTweets.Add(each);
-                                            tweetsDb.Tweets.Add(new Tweets
-                                            {
-                                                Content = each.Text,
-                                                CreatedAt = each.CreatedAt,
-                                                Language = each.Lang,
-                                                Name = each.User.Name,
-                                                Query = query,
-                                                StatusID = each.StatusID,
-                                                TweetLink = "https://twitter.com/" + each.User.Identifier.ScreenName + "/statuses/" + each.StatusID,
-                                                UserID = each.User.Identifier.ID,
-                                                Username = each.User.Identifier.ScreenName,
-                                                DateInsertedInDB = DateTime.Now
-                                            });
-                                            tweetsDb.SaveChanges();
-                                        }
-                                    }
-                                    maxId = (ulong.Parse(current[current.Count - 1].StatusID) - 1).ToString(CultureInfo.InvariantCulture);
-                                }
-                                else
-                                {
-                                    checker = false;
-                                }
-                            }
-                        }
-                        return additionalTweets;
-                    };
-                };
-            }
-            catch (Exception e)
-            {
-                throw new Exception("An error occured. Please check your network connection. " + e.Message);
-            }
-        }
+        //public List<Status> Get(string query, string sinceId)
+        //{
+        //    try
+        //    {
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw new Exception("An error occured. Please check your network connection. " + e.Message);
+        //    }
+        //}
     }
 }
--- RetrieveTwitterFeeds/Scripts/home-tweetsnodb.js
+++ RetrieveTwitterFeeds/Scripts/home-tweetsnodb.js
@@ -1,16 +1,30 @@
 ﻿$(document).ready(function () {
-    $('button').children('.loader').toggle(); //hides the loader
-    $('#tblResult').toggle(); //hides the table
+    $('button').children('.loader').toggle();
+    $('#tblResult').toggle();
 
     $('button').click(function () {
         $('button').children('.glyphicon').toggle();
         $('button').children('.loader').toggle();
-
-        $.ajax('api/gettweets', {
+        $('button').addClass('disabled');
+        
+        $.ajax('/api/gettweetsnodb', {
             method: 'GET',
-            success: function () {
-                console.log('success!');
+            data: { 'query': $('#search_term').val() },
+            success: function (data) {
+                console.dir(data);
+                $('#tblResult').toggle();
+                
+                for (var ctr = 0 ; ctr < data.length ; ctr++) { //data[ctr].CreatedAt
+                    $('#tbodyResult').append('<tr><td>' + data[ctr].StatusID  + '</td><td>' + data[ctr].User.Identifier.ScreenName + '</td><td>' + data[ctr].Text + '</td></tr>');
+                }
+            },
+            fail: function () {
+                console.log('fail');
             }
+        }).done(function () {
+            $('button').removeClass('disabled');
+            $('button').children('.glyphicon').toggle();
+            $('button').children('.loader').toggle();
         });
     });
 });
